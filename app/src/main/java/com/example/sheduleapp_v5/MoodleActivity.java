c74586d4e09ee4ae43195ce147be183e5c50e3e3
Ximelay <iliaimax64@gmail.com>
2025-05-01 17:47:35 +0800

    Moodle add

--- app/src/main/java/com/example/sheduleapp_v5/MoodleActivity.java
+++ app/src/main/java/com/example/sheduleapp_v5/MoodleActivity.java
@@ -1,7 +1,8 @@
 package com.example.sheduleapp_v5;
 
 import android.os.Bundle;
-
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -15,6 +16,20 @@ public class MoodleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_moodle);
+
+        WebView webView = findViewById(R.id.moodle_webview);
+        webView.getSettings().setJavaScriptEnabled(true); // Включите JavaScript для корректной работы Moodle
+        webView.getSettings().setDomStorageEnabled(true); // Для хранения данных сессии
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url); // Обрабатывать все ссылки внутри WebView
+                return true;
+            }
+        });
+        webView.loadUrl("https://irkpo.ru/moodle/"); // Загрузка страницы Moodle
+
+        // Обработка отступов для системных панелей
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
