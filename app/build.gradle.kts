329523e722b5a819b1eecf9049cd6dd271c48337
Ximelay <iliaimax64@gmail.com>
2025-05-06 00:56:10 +0800

    apk v1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.example.sheduleapp_v5"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.2"
+        versionCode = 2
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
