d23d18155652e3ca9059c21af4f43618cdf2a524
Ximelay <iliaimax64@gmail.com>
2025-09-08 23:25:00 +0800

    apk v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,14 +28,14 @@ if (localPropertiesFile.exists()) {
 
 android {
     namespace = "com.example.irkpo_management"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.irkpo_management"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.0.0"
+        versionCode = 11
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
